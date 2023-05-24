c6b7cb71c0a53f3184e7834e91e3f3ddadea1f70
fact <adityanarkhede2001@gmail.com>
2023-05-24 23:11:18 +0530

    Helper Contract Updated

--- contracts/Helper.sol
+++ contracts/Helper.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.8.18;
 
 contract Helper {
+ mapping(address => bool) userEligible;
 
+ function isUserEligible(address user) public view returns(bool) {
+    return userEligible[user];
+ }
+ function setUserEligible(address user) public {
+    userEligible[user] = true;
+ }
+
+ fallback() external {}
 }
